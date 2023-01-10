c94a48649219e0df515f2e5e7fb5309b9c43bb31
kyohei682474 <118451830+kyohei682474@users.noreply.github.com>
2023-01-10 13:32:40 +0000

    book

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/top' => 'homes#top'
-  get  'books/new'
+  get 'books' => 'books#index'
   post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id/' => 'books#update', as: 'update_book'
+ 
+
+
+
 end
